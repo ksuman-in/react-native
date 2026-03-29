4a4ebc2f25e33bda741a212a80a0222004978d7a
Suman Kumar <suman845415@gmail.com>
2026-03-30 00:02:36 +0530

    id not react props that issue fixed

--- app/(tabs)/subscriptions/[id].tsx
+++ app/(tabs)/subscriptions/[id].tsx
@@ -1,11 +1,12 @@
+import { useLocalSearchParams } from "expo-router";
 import { styled } from "nativewind";
-import React from "react";
 import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
-const SubscriptionDetails = ({ id }: { id: string }) => {
+const SubscriptionDetails = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Subscription Details {id}</Text>
